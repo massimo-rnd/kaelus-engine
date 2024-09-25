82e5c73cce96853c7a86fea67b35e96e2a4660e0
Christoph <christoph@rustige.me>
2024-09-25 23:21:09 +0200

    implemented functionality
    - Implemented functionality from email-extractor as a base
    - finally implemented "url fixing"
    - added Kaleidolib
    - implemented Logoprinter

---
+++ engine/LogoPrinter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaelusengine.engine
+{
+    internal class LogoPrinter
+    {
+        internal static void PrintLogo()
+        {
+            Console.WriteLine("####    #####         ####           ##########       ####            ####     ####        #######  ");
+            Console.WriteLine("####  #####          ######          ##########       ####            ####     ####      ###########");
+            Console.WriteLine("##########           #######                          ####            ####     ####      ####    #  ");
+            Console.WriteLine("########            ########         ##########       ####            ####     ####       #######   ");
+            Console.WriteLine("######             ####  ####        ##########       ####            ####     ####         ########");
+            Console.WriteLine("####              ####    ####                        ####            ####    #####       ##    ####");
+            Console.WriteLine("####   ####      ######### ####      ##########       ###########      ###########      ############");
+            Console.WriteLine("####    #####    ####  ##  ####      ##########       ###########        #######          ########  ");
+            Console.WriteLine("\n\n\n");
+        }
+    }
+}
